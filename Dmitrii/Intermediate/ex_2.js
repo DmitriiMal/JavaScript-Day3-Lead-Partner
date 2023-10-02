ea8ce3d90862cba4d81787b503427ce4c7a71818
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-02 13:47:05 +0200

    complete ex 2 Intermediate

---
+++ Dmitrii/Intermediate/ex_2.js
@@ -0,0 +1,16 @@
+// ///////////////////////////////////// //
+// ////////// ex 2 | FizzBuzz ////////// //
+// ///////////////////////////////////// //
+
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(`${i} FizzBuzz`);
+  } else if (i % 3 == 0) {
+    console.log(`${i} Fizz`);
+  } else if (i % 5 == 0) {
+    console.log(`${i} Buzz`);
+  } else {
+    console.log(i);
+  }
+}
